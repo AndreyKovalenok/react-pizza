299e4bfe6f4bb322754786a5d47f6f007cf2bd25
Andrey <a.kovalenok679@gmail.com>
2020-10-12 19:46:54 +0200

    Добавлен stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extend: "stylelint-config-sass-guidelines",
+  rules: {
+    "indentation": "tab",
+    "number-leading-zero": null,
+    "color-hex-case": "upper",
+    "color-hex-length": "long",
+  }
+}
