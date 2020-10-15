ce523df0746edd5b60e124687718d7d2a6e55215
Andrey <a.kovalenok679@gmail.com>
2020-10-15 21:15:57 +0200

    Верстка табы

---
+++ src/components/UI/Tab/index.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+interface Props {
+  active: boolean;
+}
+
+const Tab = styled.button<Props>`
+  margin: 0 0.2rem;
+  padding: 1.4rem 2.5rem 1.6rem;
+  font-size: var(--font-size-3);
+  line-height: var(--line-height-2);
+  font-weight: var(--bold-weight);
+  color: ${(p) => (p.active ? "var(--color-white)" : "var(--color-gray-4)")};
+  background-color: ${(p) =>
+    p.active ? "var(--color-gray-5)" : "var(--color-gray-6)"};
+  border-radius: 3rem;
+`;
+
+export default Tab;
--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,7 +1,48 @@
 import React from "react";
+import styled from "styled-components";
+
+import Tab from "../../components/UI/Tab";
+
+const Section = styled.section`
+  padding: 4rem 5.3rem 9.6rem 6.7rem;
+`;
+
+const Controls = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Tabs = styled.ul`
+  display: flex;
+  justify-content: flex-start;
+  margin: 0 -0.4rem;
+`;
 
 function MainPage(): JSX.Element {
-  return <section>123</section>;
+  const tabs = [
+    { id: 0, text: "Все" },
+    { id: 1, text: "Мясные" },
+    { id: 2, text: "Вегетарианская" },
+    { id: 3, text: "Гриль" },
+    { id: 4, text: "Острые" },
+    { id: 5, text: "Закрытые" },
+  ];
+
+  return (
+    <Section>
+      <Controls>
+        <Tabs>
+          {tabs.map(({ id, text }, index) => {
+            return (
+              <li key={id}>
+                <Tab active={index === 0}>{text}</Tab>
+              </li>
+            );
+          })}
+        </Tabs>
+      </Controls>
+    </Section>
+  );
 }
 
 export default MainPage;
