e10dfa1f14dbf5eb7edbc432662f8a7d3a8c89e5
Andrey <a.kovalenok679@gmail.com>
2020-10-20 21:09:44 +0200

    Страница корзины связана с redux

--- src/pages/BasketPage/index.tsx
+++ src/pages/BasketPage/index.tsx
@@ -1,16 +1,23 @@
 import React from "react";
+import { connect, ConnectedProps } from "react-redux";
 import styled from "styled-components";
 
 import EmptyBasket from "./EmptyBasket";
+import { IState } from "../../redux/rootReducer";
+import { IBasketItem } from "../../redux/actions/basket";
 
 const Section = styled.section``;
 
-function BasketPage(): JSX.Element {
-  return (
-    <Section>
-      <EmptyBasket />
-    </Section>
-  );
+const mapState = ({ basket }: IState) => {
+  return { basket };
+};
+
+const connector = connect(mapState);
+type PropsFromRedux = ConnectedProps<typeof connector>;
+type Props = PropsFromRedux & IBasketItem;
+
+function BasketPage({ basket: { pizzasList } }: Props): JSX.Element {
+  return <Section>{!pizzasList.length && <EmptyBasket />}</Section>;
 }
 
-export default BasketPage;
+export default connector(BasketPage);
