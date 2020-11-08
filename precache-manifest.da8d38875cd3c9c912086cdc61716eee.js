daeaf349404190724d1682e74802a5e8834f8e4e
Andrey <a.kovalenok679@gmail.com>
2020-11-08 18:22:21 +0200

    Updates

---
+++ precache-manifest.da8d38875cd3c9c912086cdc61716eee.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d308e426e2415b9e6d90c5c8d8ede0f",
+    "url": "//github.com/AndreyKovalenok.github.io/react-pizza/index.html"
+  },
+  {
+    "revision": "068a9f9655d7d07f88a1",
+    "url": "//github.com/AndreyKovalenok.github.io/react-pizza/static/css/main.6e689ccb.chunk.css"
+  },
+  {
+    "revision": "0b9b851c8a98f59c857b",
+    "url": "//github.com/AndreyKovalenok.github.io/react-pizza/static/js/2.b795c77d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "//github.com/AndreyKovalenok.github.io/react-pizza/static/js/2.b795c77d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "068a9f9655d7d07f88a1",
+    "url": "//github.com/AndreyKovalenok.github.io/react-pizza/static/js/main.ef8f74e9.chunk.js"
+  },
+  {
+    "revision": "698ed2231986d86eaee4",
+    "url": "//github.com/AndreyKovalenok.github.io/react-pizza/static/js/runtime-main.6c9ea341.js"
+  },
+  {
+    "revision": "e2cf3dc2f079bf3d5185a02552f153c4",
+    "url": "//github.com/AndreyKovalenok.github.io/react-pizza/static/media/ProximaNova-Bold.e2cf3dc2.woff"
+  },
+  {
+    "revision": "ccf59c77109aecc8deeeede82e3bac87",
+    "url": "//github.com/AndreyKovalenok.github.io/react-pizza/static/media/ProximaNova-Extrabld.ccf59c77.woff"
+  },
+  {
+    "revision": "2d2ae2556b24a45ff8d5ed86b07b5783",
+    "url": "//github.com/AndreyKovalenok.github.io/react-pizza/static/media/ProximaNova-Regular.2d2ae255.woff"
+  },
+  {
+    "revision": "a1b6992ac4bd4e78cf01f811308743c6",
+    "url": "//github.com/AndreyKovalenok.github.io/react-pizza/static/media/arrow.a1b6992a.svg"
+  },
+  {
+    "revision": "5d7ef77b3ccc0b384d7b33cb4ba94dcd",
+    "url": "//github.com/AndreyKovalenok.github.io/react-pizza/static/media/basket-clear.5d7ef77b.svg"
+  },
+  {
+    "revision": "118838167f52e918fc044b97cc42e081",
+    "url": "//github.com/AndreyKovalenok.github.io/react-pizza/static/media/basket-icon.11883816.svg"
+  },
+  {
+    "revision": "cb31982108d245211611a77f5afcb924",
+    "url": "//github.com/AndreyKovalenok.github.io/react-pizza/static/media/basket.cb319821.svg"
+  },
+  {
+    "revision": "c6fdc55725637e34b900b8f2810b2e3a",
+    "url": "//github.com/AndreyKovalenok.github.io/react-pizza/static/media/dark-basket-icon.c6fdc557.svg"
+  },
+  {
+    "revision": "7036d3ff4ec3f93e2c40f2b11355db47",
+    "url": "//github.com/AndreyKovalenok.github.io/react-pizza/static/media/dropdown-arrow.7036d3ff.svg"
+  },
+  {
+    "revision": "3bf5a3d74e3e4cdb104553a18da827b6",
+    "url": "//github.com/AndreyKovalenok.github.io/react-pizza/static/media/emoji.3bf5a3d7.svg"
+  },
+  {
+    "revision": "0491805c996e5b675ce812740d54fff8",
+    "url": "//github.com/AndreyKovalenok.github.io/react-pizza/static/media/image-0.0491805c.png"
+  },
+  {
+    "revision": "1363a3d5c3d3bbaad6edffdef3621ffa",
+    "url": "//github.com/AndreyKovalenok.github.io/react-pizza/static/media/image-1.1363a3d5.png"
+  },
+  {
+    "revision": "c89cd349aefdeb62df6fa0295c27996d",
+    "url": "//github.com/AndreyKovalenok.github.io/react-pizza/static/media/image-2.c89cd349.png"
+  },
+  {
+    "revision": "368436e543d287e73d7746a300859ae3",
+    "url": "//github.com/AndreyKovalenok.github.io/react-pizza/static/media/image-3.368436e5.png"
+  }
+]);
